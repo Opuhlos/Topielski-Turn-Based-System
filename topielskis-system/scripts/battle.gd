297df24888ae15ce6678e0b8bf18ed8b41b573e6
Eric Nguyen <ericnguyen455@gmail.com>
2025-08-01 21:40:37 -0400

    enemy attack

--- topielskis-system/scripts/battle.gd
+++ topielskis-system/scripts/battle.gd
@@ -5,15 +5,16 @@ signal textbox_closed
 @export var enemy: BaseEnemy
 
 @onready var enemy_hp_bar: ProgressBar = $EnemyContainer/EnemyHealthBar
+@onready var player_hp_bar: ProgressBar = $PlayerPanel/MarginContainer/PlayerData/PlayerHealthBar
 
 var current_player_health: int
 var current_enemy_health: int
 
 func _ready() -> void:
-	set_health($PlayerPanel/MarginContainer/PlayerData/PlayerHealthBar, State.max_health, State.max_health)
+	set_health(player_hp_bar, State.max_health, State.max_health)
 	current_player_health = State.max_health
 	
-	set_health($EnemyContainer/EnemyHealthBar, enemy.health, enemy.health)
+	set_health(enemy_hp_bar, enemy.health, enemy.health)
 	$EnemyContainer/Enemy.texture = enemy.texture
 	current_enemy_health = enemy.health
 	
@@ -42,6 +43,25 @@ func display_text(text: String) -> void:
 	$Textbox.show()
 	$Textbox/Panel/MarginContainer/Text.text = text
 
+func set_health(progress_bar: ProgressBar, health: int, max_health: int) -> void:
+	progress_bar.max_value = max_health
+	progress_bar.value = health
+	
+	var label: Label = progress_bar.get_node("Label")
+	label.text = "HP %s/%s" % [health, max_health]
+
+func enemy_turn() -> void:
+	await get_tree().create_timer(1).timeout
+	
+	display_text("The %s attacked you!" % [enemy.name])
+	await textbox_closed
+	
+	$AnimationPlayer.play("shake")
+	await $AnimationPlayer.animation_finished
+	
+	current_player_health = max(0, current_player_health - enemy.attack)
+	set_health(player_hp_bar, current_player_health, State.max_health)
+
 func _on_pressed_run_button() -> void:
 	display_text("You escaped!")
 	await textbox_closed
@@ -51,16 +71,10 @@ func _on_pressed_attack_button() -> void:
 	display_text("You attacked!")
 	await textbox_closed
 	
+	$AnimationPlayer.play("enemy_damaged")
+	await $AnimationPlayer.animation_finished
+	
 	current_enemy_health = max(0, current_enemy_health - State.damage)
-	
-	display_text("%s took %s damage!" % [enemy.name, State.damage])
-	await textbox_closed
-	
 	set_health(enemy_hp_bar, current_enemy_health, enemy.health)
-
-func set_health(progress_bar: ProgressBar, health: int, max_health: int) -> void:
-	progress_bar.max_value = max_health
-	progress_bar.value = health
 	
-	var label: Label = progress_bar.get_node("Label")
-	label.text = "HP %s/%s" % [health, max_health]
+	enemy_turn()
